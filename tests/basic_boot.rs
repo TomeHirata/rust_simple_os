23d6352466250bfcc7d6878ecfa3296630b6ddd2
TomeHirata <tomu.hirata@gmail.com>
2022-04-03 16:43:45 +0900

    chore: fix test cases

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -8,6 +8,13 @@ use rust_os::println;
 
 use core::panic::PanicInfo;
 
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rust_os::test_panic_handler(info)
